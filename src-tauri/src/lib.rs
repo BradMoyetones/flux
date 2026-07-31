93980cb732a44345def5afbc3ef7204ac309fd4a
Brad Moyetones <brad.moyetones@gmail.com>
2026-07-31 16:50:11 -0500

    feat: add and enable tauri-plugin-dialog for file system operations

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -15,6 +15,7 @@ pub fn run() {
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .setup(|app| {
             // Obtenemos la ventana nativa de Tauri v2
